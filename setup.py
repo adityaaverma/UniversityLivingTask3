08eb3cb34d99601aec31edf9d5cf26931bf840b1
adityaaverma <91687122+adityaaverma@users.noreply.github.com>
2024-06-11 15:16:55 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Medical Chatbot',
+    version='0.0.0',
+    author='Aditya Aryan Verma',
+    author_email='adityaverma655@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
